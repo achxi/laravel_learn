b5790b5e0eb8c18e3b5f5c876a51f9fc20a15295
achxi <achxi88@gmail.com>
2014-10-18 00:49:36 +0700

    Inittial commit

---
+++ app/tests/AssertTest.php
@@ -0,0 +1,22 @@
+<?php
+
+class AssertTest extends TestCase {
+
+	/**
+	 * A basic functional test example.
+	 *
+	 * @return void
+	 */
+	public function testAssert()
+	{
+/*		$theTruth = true;
+		$this->assertFalse($theTruth);*/
+
+/*		$theString = 'Roux Academy of Art and Science';
+		$this->assertSame('Roux Academy of Art and Science', $theString);*/		
+
+		$theString = 'Roux Academy of Art and Science';
+		$this->assertContains('Art', $theString);
+	}
+
+}
